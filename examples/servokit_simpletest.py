2bf4e3681b57dbb6ad0e1e441a64ad47b133fc93
Mark Vogt <markevogt@outlook.com>
2020-04-30 19:16:20 -0500

    INSTALLED envSERVOKIT... MODIFIED simpletest.py... WORKING servo :) WORKING (partially) motor :-) ...

--- examples/servokit_simpletest.py
+++ examples/servokit_simpletest.py
@@ -4,12 +4,26 @@ from adafruit_servokit import ServoKit
 
 # Set channels to the number of servo channels on your kit.
 # 8 for FeatherWing, 16 for Shield/HAT/Bonnet.
-kit = ServoKit(channels=8)
+kit = ServoKit(channels=16)
 
-kit.servo[0].angle = 180
+# ARM the motor on channel 1...
 kit.continuous_servo[1].throttle = 1
-time.sleep(1)
 kit.continuous_servo[1].throttle = -1
-time.sleep(1)
-kit.servo[0].angle = 0
 kit.continuous_servo[1].throttle = 0
+
+# ACTIVATE servo on channel 0 to its 180 deg setting...
+kit.servo[0].angle = 180
+# ACTIVATE motor on channel 1 to 100% ?...
+kit.continuous_servo[1].throttle = 1
+# PAUSE 1 second...
+time.sleep(1)
+
+# ACTIVATE servo on channel 0 to it 0 deg setting...
+kit.servo[0].angle = 0
+# ACTIVATE motor on channel 1 to 0%...
+kit.continuous_servo[1].throttle = 0
+# PAUSE 1 second...
+time.sleep(1)
+
+
+
