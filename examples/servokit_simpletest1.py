fa83521c109ccb60932f61b5f730a98198a1c243
Mark Vogt <markevogt@outlook.com>
2020-05-06 11:31:31 -0500

    FINALIZED both simpletest files...

--- examples/servokit_simpletest1.py
+++ examples/servokit_simpletest1.py
@@ -6,22 +6,21 @@ from adafruit_servokit import ServoKit
 # 8 for FeatherWing, 16 for Shield/HAT/Bonnet.
 kit = ServoKit(channels=16)
 
-# ARM the motor on channel 1...
+# SEND MAX THROTTLE (1) command to motor...
 kit.continuous_servo[1].throttle = 1
-time.sleep(1)
+time.sleep(2)
+# SET motor to 50% with a (.5) command...
+kit.continuous_servo[1].throttle = .5
+time.sleep(2)
+# STOP motor with a STOP THROTTLE (-1) command...
 kit.continuous_servo[1].throttle = -1
-time.sleep(1)
-kit.continuous_servo[1].throttle = 0
+time.sleep(3)
 
 # ACTIVATE servo on channel 0 to its 180 deg setting...
 kit.servo[0].angle = 180
-# ACTIVATE motor on channel 1 to 100% ?...
-kit.continuous_servo[1].throttle = .1
 # PAUSE 1 second...
 time.sleep(1)
-
 # ACTIVATE servo on channel 0 to it 0 deg setting...
 kit.servo[0].angle = 0
-# ACTIVATE motor on channel 1 to 0%...
-kit.continuous_servo[1].throttle = 0
-kit.continuous_servo[1].throttle = 0
+time.sleep(2)
+
